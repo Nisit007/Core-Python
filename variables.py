12ffef57df858379ea7df6fbd163479f2bc4073e
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-02 10:47:43 -0400

    variables.py

---
+++ variables.py
@@ -0,0 +1,4 @@
+        ###How to write variable###
+name = "Nisit"
+print(name)   ......>## 'name' is a variable 
+....>condition is to create variable you can not a same name builtin function<......
