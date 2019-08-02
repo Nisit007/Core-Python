1d9422234fe7359ba31eae92dcf36d9652eb1f72
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-02 10:39:27 -0400

    py_intro.py
    In this file you can simple print name and how to comment in any line.

---
+++ py_intro.py
@@ -0,0 +1,9 @@
+        ###Hello world print###
+print("Hello world")
+
+        ###Input Method###
+name = input("enter your name")
+
+        ###comments in python###
+1)You can use this '#' symbol to comment any line         
+2)ctrl+/
