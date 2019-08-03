14fd0369bd6a6a2df6c0497bd9486a61ea83666f
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-03 08:12:31 -0400

    Update String_Formatting.py

--- String_Formatting.py
+++ String_Formatting.py
@@ -1,3 +1,7 @@
 name = "Nishit"
 age = 23
 print(f"your name is {name} your age is {age}")
+            
+  ##Let's see the example
+  num1,num2,num3 = int(input("Enter three number sapereted by comma")).split(',')
+  print(f"The average of three num is {(int(num1)+int(num2)+int(num3))/3}")
