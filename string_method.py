1bc9426c6ef7cd8fdf9d979a9d2e9eacb10dbca8
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-05 07:05:59 -0400

    Create string_method.py

---
+++ string_method.py
@@ -0,0 +1,11 @@
+name = "Nishit Patel"
+length = len(name)
+print(length)
+    #lower method
+print(name.lower())#--->string will be converted in to lower case
+    #upper method
+print(name.lower())#--->string will be converted in to upper case
+    #title method
+print(name.title())#--->first latter of every string will converted into uppercase
+    #count method
+print(name.count("i"))#--->count latter in the string
