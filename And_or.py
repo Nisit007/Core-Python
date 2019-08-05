011b4da10171d4d710144998aedad92a8ec7d4a3
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-05 07:50:02 -0400

    Create And_or.py

---
+++ And_or.py
@@ -0,0 +1,21 @@
+name = "Nishit"
+age = 23
+if name == "Nishit" and age == 23
+  print("Condition is True")        #--->out put is condition true
+else:
+  print("Condition False")
+  
+if name == "Nishit" and age == 19
+  print("Condition is True")        #--->out put is condition False
+else:
+  print("Condition False")
+
+if name == "Nishit" or age == 19
+  print("Condition is True")        #--->out put is condition true
+else:
+  print("Condition False")
+  
+if name == "Ravi" and age == 19
+  print("Condition is True")        #--->out put is condition False
+else:
+  print("Condition False")
