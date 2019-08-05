d5f6795696075616d18544e7a3d7bc6f2df5d395
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-05 07:14:23 -0400

    Update find.py

--- find.py
+++ find.py
@@ -1,2 +1,3 @@
-name = "Hello my name is nishit"
+name = "Hello my name is nishit and nishit is good"
 print(name.find("is")) #--->it will be show the index and this index will be shown first 
+print(name.find("is",2)) #--->it will be show the second "is" index
