21ee4337a8daaf9f499f299225700c071c6c7d35
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-05 07:23:01 -0400

    Update center.py

--- center.py
+++ center.py
@@ -1,2 +1,5 @@
 name = "nishit"
 print(name.center(8,"*")) #--->num of '*' = length of string + add '*' you want to add
+# --->with using input method
+name1 = input("enter your name")
+print(name1.center(len(name)+8,"*"))
