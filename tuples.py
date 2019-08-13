33716653d799bb03bcdc90effc29ee3d466f14ba
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-13 03:13:19 -0400

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,4 @@
+ex = ('orange','banana','apple')
+# no append,no pop,no remove,no append,no insert because tuples are immutable
+# only count,index,length,slicing methods are applied
+print(ex[:2]) #--->output = ('orange','banana')
