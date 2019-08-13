8bda2d0db97f36daebbc19b135b41677979b4a0d
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-13 02:25:22 -0400

    Create list_methods.py

---
+++ list_methods.py
@@ -0,0 +1,33 @@
+a = [1,2,3,'nishit']
+b = [4,5,6]
+
+            # POP method
+# print('original list = ' , a)
+# print('deleted value = ' ,a.pop(2))
+# print(a.pop())
+
+            # TYPE method
+# print('show type of list' ,type(a))
+
+            # REVERSE method
+# print('original list = ' , b)
+# b.reverse()
+# print('reverse list = ' ,b)
+
+            # APPEND method
+# a.append(444)
+# print('new adding value' ,a)
+
+            # CLEAR method
+# a.clear()
+# print(a)
+
+            # INDEX method
+# print('show position of value =' ,a[3])
+        
+            # SLICE method
+print(b[1:4])
+      
+            # SHORT method
+b.sort()
+print(b)
