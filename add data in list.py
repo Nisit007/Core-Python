035edc1559ce37e655a6e323c92e88f85a6a5cf5
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-19 15:50:36 +0530

    Create add data in list.py

---
+++ add data in list.py
@@ -0,0 +1,8 @@
+detail = ['apple','banana']
+detail.append('cherry')
+print(detail) #output-['apple','banana','cherry']
+new = []
+new.append('apple')
+new.append('banana')
+new.append('cherry')
+print(new)  #output-['apple','banana','cherry']
