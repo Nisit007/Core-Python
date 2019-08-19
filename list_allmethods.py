6d70beaa6b6fc0da6a5bc3318b4b495a4dc13b76
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-08-19 15:53:47 +0530

    Rename list_methods.py to list_allmethods.py

---
+++ list_allmethods.py
@@ -0,0 +1,34 @@
+a = [1,2,3,'nishit']
+b = [4,5,6]
+      
+            #pop method
+print('original list = ' , a)
+# print('deleted value = ' ,a.pop(2))
+# print(a.pop())
+
+            # TYPE method
+# print('show type of list' ,type(a))
+
+            # REVERSE method
+# print('original list = ' , b)
+# b.reverse()
+# print('reverse list = ' ,b)
+
+            # APPEND method
+# a.append(444)
+# print('new adding value' ,a)
+
+            # CLEAR method
+# a.clear()
+# print(a)
+
+            # INDEX method
+# print('show position of value =' ,a[3])
+
+            # SLICE method
+print(b[1:4])
+
+            # SORTING list method
+a = [10,5,30,20,46,58]
+a.sort()
+print(a)
