0774bd2c7999c57293a6989ed2d0af78805b8909
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-09-23 11:51:09 +0530

    Delete add data in list.py

--- add data in list.py
+++
@@ -1,8 +0,0 @@
-detail = ['apple','banana']
-detail.append('cherry')
-print(detail) #output-['apple','banana','cherry']
-new = []
-new.append('apple')
-new.append('banana')
-new.append('cherry')
-print(new)  #output-['apple','banana','cherry']
