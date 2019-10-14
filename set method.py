62d0b88db5e9c7cecded25ab2c13d0114ab50eff
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-10-14 11:29:59 +0530

    Add files via upload

---
+++ set method.py
@@ -0,0 +1,31 @@
+x = {'apple', 'banana', 'kiwi'}
+y = {'apple', 'banana', 'mango', 'orange', 'kiwi'}
+# print(x)
+
+# ADD method
+# y.add(999)
+# print(y)
+
+# CLEAR method
+# x.clear()
+# print(x)
+
+# COPY method
+# z = x.copy()
+# print('z = ', z)
+
+# DIFFERENCE method
+# z = y.difference(x)
+# print(z)
+
+# DISCARD method
+# x.discard('apple')
+# print(x)
+
+# INTERSECTION method
+# z = y.intersection(x)
+# print(z)
+
+# ISSUBSET method
+# z = y.issubset(x)
+# print(z)
