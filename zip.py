c674f331a493db05cba50c255f025ca1c41ce2d5
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-10-14 11:33:50 +0530

    Add files via upload

---
+++ zip.py
@@ -0,0 +1,12 @@
+a = ("nishit", "samarth", "urvesh")
+b = ("patel", "bhavnagar", "ahmedabad")
+# z = a + b
+# print(z)
+# (('nishit','patel'),('samarth','bhavnagar'))
+
+# x = zip(a, b)
+# print(tuple(x))
+
+
+# x = zip(a, b)
+# print(list(x))
