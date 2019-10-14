e19c8563a5ddd6dd3520d7c219453976b874feca
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-10-14 11:38:52 +0530

    Add files via upload

---
+++ file_handling.py
@@ -0,0 +1,21 @@
+            # create new file
+# f = open("djnago tut.txt", "x")
+# print(f)
+
+            # Write into file
+# f = open("django tut1.txt","a")
+# f.write("Now file will be filled")
+# f.close()
+
+
+            # file open and read
+# f = open("django tut1.txt", "r")
+# print(f.read())
+
+
+           # Delete files
+# import os
+# if os.path.exists("django tut1.txt"):
+#     os.remove("django tut1.txt")
+# else:
+#     print("File does not exist")
