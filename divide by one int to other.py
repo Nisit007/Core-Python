d2f3e0aa3d82f95365086b8147ed30f4c91c26c6
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-10-24 11:21:04 +0530

    Add files via upload

---
+++ divide by one int to other.py
@@ -0,0 +1,7 @@
+a = int(input('enter the number:- '))
+b = int(input('enter the number:- '))
+if a % b == 0:
+    print('it is possible')
+else:
+    print('this is not possible')
+    print("remainder is = ", a % b)
