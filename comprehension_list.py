9f2a5e1a71962d3b5c2d4ec344f8c78771b2e73a
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2019-12-09 13:52:59 +0530

    Create comprehension_list.py

---
+++ comprehension_list.py
@@ -0,0 +1,29 @@
+# numbers = [1, 2, 3, 4, 4, 5, 5, 6, 6, 74, 5]
+# blank = []
+# for i in numbers:
+#     if i % 2 == 0:
+#         blank.append(i)
+# print(blank)
+
+# using comprehension
+# blank = [i for i in numbers if i % 2 == 0]
+# print(blank)
+
+# z = [i for i in numbers if i % 2 == 0]
+# print(z)
+
+
+# state = ['Gujarat', 'Maharashtra', 'Rajasthan']
+# capital = ['Gandhinagar', 'Mumbai', 'Jaipur']
+# blank = {}
+# blank = {key: value for (key, value) in zip(state, capital)}
+# print(blank)
+#
+# for (key, value) in zip(state, capital):
+#     blank[key] = value
+# print(blank)
+
+
+# print(dict(zip(state, capital)))
+
+
