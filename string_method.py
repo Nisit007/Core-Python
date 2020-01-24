37dd93c32eda81eb062c3287204375a7d42d9300
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2020-01-24 19:55:45 +0530

    Update string_method.py

--- string_method.py
+++ string_method.py
@@ -4,7 +4,7 @@ print(length)
     #lower method
 print(name.lower())#--->string will be converted in to lower case
     #upper method
-print(name.lower())#--->string will be converted in to upper case
+print(name.upper())#--->string will be converted in to upper case
     #title method
 print(name.title())#--->first latter of every string will converted into uppercase
     #count method
