20862ec672bd0b483b2f3b5e9d300610d3d00162
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2020-03-11 13:04:03 +0530

    Add files via upload

---
+++ middlenum.py
@@ -0,0 +1,11 @@
+# a = int(input('enter the number of A '))
+# b = int(input('enter the number of B '))
+# c = int(input('enter the number of C '))
+# if b > a and a > c or c > a and a > b:
+#     print(a)
+# if a > b and b > c or c > b and b > a:
+#     print(b)
+# if a > c and c > b or b > c and c > a:
+#     print(c)
+
+
